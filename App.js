bece8fb5df8c173ccde2d2fcd2a64ad09e3d0f05
Ray <41517811+rayabelcode@users.noreply.github.com>
2024-12-07 13:39:37 -0500

    Sentry Support Added
    -Added Sentry.io

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
+// Polyfill for URL support
 import 'react-native-url-polyfill/auto';
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/context/AuthContext';
 import TabNavigator from './src/navigation/TabNavigator';
 import { LogBox } from 'react-native';
+import * as Sentry from '@sentry/react-native';
+import Constants from 'expo-constants';
+
+// Initialize Sentry
+Sentry.init({
+  dsn: Constants.expoConfig?.extra?.SENTRY_DSN // Use expoConfig for production builds
+    || Constants.manifest?.extra?.SENTRY_DSN, // Fallback to manifest for development mode
+  enableNative: false,
+  debug: true // Set to false in production to disable debug logging
+});
 
 // Ignore specific Firebase warnings
 LogBox.ignoreLogs([
